a10c9f59450c62dbebf75074304c843992263b8c
TobiasComp <tuvia1234@gmail.com>
2019-11-07 17:29:37 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-rwcv8p
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-rwcv8p)
---
+++ src/app/clock/clock.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'my-clock',
+  templateUrl: './clock.component.html',
+  styleUrls: ['./clock.component.css']
+})
+export class ClockComponent  {
+  time:string;
+  @Input () city:string;
+  @Input () bg:string;
+
+  constructor() { 
+    setInterval(()=>this.setTime(),1000)
+  }
+
+  
+  setTime(){
+      var today = new Date();
+      this.time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+       
+  }
+  
+}
